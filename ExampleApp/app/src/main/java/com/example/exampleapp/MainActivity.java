b220954777e0f64246526988c1f08de58eb0d866
marcelojaf <marcelojaf@gmail.com>
2020-09-06 16:38:04 -0400

    Fixing wrong variable place

--- ExampleApp/app/src/main/java/com/example/exampleapp/MainActivity.java
+++ ExampleApp/app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -6,11 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText txtName = (EditText) findViewById(R.id.txtLogin);
-    private EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +17,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickFunction(View view){
+        EditText txtName = (EditText) findViewById(R.id.txtLogin);
+        EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+
         Log.i("Login", txtName.getText().toString());
         Log.i("Password", txtPassword.getText().toString());
+
+        Toast.makeText(this, "Testing Toast!", Toast.LENGTH_SHORT).show();
     }
 }
